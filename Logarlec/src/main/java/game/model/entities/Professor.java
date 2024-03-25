f51481a2b333022ccf191bfcd4741273bad8f404
fejervaridori <144676980+fejervaridori@users.noreply.github.com>
2024-03-25 11:29:08 +0100

    Update Professor.java

--- Logarlec/src/main/java/game/model/entities/Professor.java
+++ Logarlec/src/main/java/game/model/entities/Professor.java
@@ -4,6 +4,7 @@ import game.model.logging.Suttogo;
 
 //#todo: implement class
 public class Professor extends Character{
+    //Egy professzor körének menete
     public void doRound(){
         Suttogo.info("doRound()");
         //ezt akkor kéne megcsinálni, amikor már a grafikus interfésszel kapcsolatos event kezelés is tálalékon lesz,
