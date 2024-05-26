bff3eb4fd839a91b5667dcaa7a7de2e7bc505850
Szokoly-Angyal Armand <128236752+FrighteningFunction@users.noreply.github.com>
2024-05-26 21:33:44 +0200

    add doc

--- src/main/java/game/model/main/GameMain.java
+++ src/main/java/game/model/main/GameMain.java
@@ -250,6 +250,15 @@ public class GameMain {
             command.execute(cmd);
         }
     }
+
+    /**
+     * A scriptes végrehajtás függvénye. A scriptet a src/main/resources/gamebuilder_scripts mappában kell elhelyezni, és a
+     * scriptPath változóban kell megadni a fájl nevét.
+     * A fájl használata: helyezz bele parancsokat úgy, mint ha a szabványos inputra írnád be őket, minden sor egy parancs.
+     * Vigyázz, mert a kezdetektől kell specifikálnod a parancsokat (szóval kell a newgame és a startgame is!)
+     * Note: alapértelmezésben a random ki van kapcsolva.
+     */
+
     public static void executeScript(){
         Suttogo.getSuttogo().info("SYSTEM started processing script, please wait...");
 
